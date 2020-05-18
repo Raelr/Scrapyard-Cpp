3c4676db42252fadac0122019add46a4eea055a8
Aryeh <aryehzinn@hotmail.com>
2020-05-19 06:46:20 +1000

    Added application events

--- Scrapyard/src/Scrapyard/Events/AppEvent.h
+++ Scrapyard/src/Scrapyard/Events/AppEvent.h
@@ -1,11 +1,13 @@
-#ifndef MOUSEEVENT_H
-#define MOUSEEVENT_H
+#ifndef APPEVENT_H
+#define APPEVENT_H
 
 #include "Event.h"
 #include <sstream>
 #include <string>
 
-class WindowResizeEvent : public Event {
+namespace Scrapyard {
+
+    class WindowResizeEvent : public Event {
     public:
         WindowResizeEvent(unsigned int width, unsigned int height) 
                 : m_Width{width}, m_Height{height} {}
@@ -23,6 +25,42 @@ class WindowResizeEvent : public Event {
         E_CLASS_CATEGORY(Application)
     private:
         unsigned int m_Width, m_Height;
+    };
+
+    class WindowCloseEvent : public Event {
+        public:
+            WindowCloseEvent() { }
+            
+            E_CLASS_TYPE(WindowClose)
+            E_CLASS_CATEGORY(Application)
+
+    };
+
+    class TickEvent : public Event {
+        public: 
+            TickEvent() { }
+
+            E_CLASS_TYPE(AppTick)
+            E_CLASS_CATEGORY(Application)
+    };
+
+    class UpdateEvent : public Event {
+        public: 
+            UpdateEvent() { }
+
+            E_CLASS_TYPE(AppUpdate)
+            E_CLASS_CATEGORY(Application)
+    };
+
+    class RenderEvent : public Event {
+        public: 
+            RenderEvent() { }
+
+            E_CLASS_TYPE(AppRender)
+            E_CLASS_CATEGORY(Application)
+    };
 }
+
+
 
 #endif
